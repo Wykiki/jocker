27fc0c273ca45c2ad6dbff22ba74a1f849e70974
Wykiki <wykiki@wyki.io>
2025-05-16 18:24:04 +0200

    feat(nix): Use fetchGit instead of fetchTarball

--- shell.nix
+++ shell.nix
@@ -1,5 +1,8 @@
 let
-  rust-overlay = (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/ce79bb52eb023f71a03e88cb36c66f35c6668a95.tar.gz"));
+  rust-overlay = (import (builtins.fetchGit {
+    url = "https://github.com/oxalica/rust-overlay.git";
+    rev = "ce79bb52eb023f71a03e88cb36c66f35c6668a95";
+  }));
   pkgs = (import <nixpkgs> {
     overlays = [ rust-overlay ];
   });
