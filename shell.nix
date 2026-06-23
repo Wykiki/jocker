83247c6d485e9f6b149a706d459ac26e1f97607b
Wykiki <wykiki@wyki.io>
2026-06-23 19:57:48 +0200

    Bump rust to 1.96

--- shell.nix
+++ shell.nix
@@ -1,15 +1,22 @@
 let
-  rust-overlay = (import (builtins.fetchGit {
-    url = "https://github.com/oxalica/rust-overlay.git";
-    rev = "99cc5667eece98bb35dcf35f7e511031a8b7a125";
-  }));
-  pkgs = (import <nixpkgs> {
+  rust-overlay = import (
+    builtins.fetchGit {
+      url = "https://github.com/oxalica/rust-overlay.git";
+      ref = "master";
+    }
+  );
+  pkgs = import <nixpkgs> {
     overlays = [ rust-overlay ];
-  });
+  };
 in
 pkgs.mkShell {
   buildInputs = [
-    (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+    ((pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+      extensions = [
+        "rust-src"
+        "rust-analyzer"
+      ];
+    })
   ];
   packages = with pkgs; [
     mold
